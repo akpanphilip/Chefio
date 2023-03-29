2eb8caae7e1994c0701d037702dbf2a4e0ed9164
akpanphilip <akpanphilip1122@gmail.com>
2023-03-29 16:13:43 +0100

    added onboarding screen

--- lib/screens/index.dart
+++ lib/screens/index.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, deprecated_member_use
 
 import 'package:flutter/material.dart';
 
@@ -12,6 +12,35 @@ class Index extends StatelessWidget {
       children: [
         Image(
           image: AssetImage("images/onboard.png"),
+        ),
+        Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Text(
+                "Start Cooking",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22),
+              ),
+              SizedBox(height: 15),
+              Text("Let's join our community", style: TextStyle(fontSize: 17)),
+              Text("to cook better food", style: TextStyle(fontSize: 17)),
+              SizedBox(height: 60),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Get Started"),
+                style: ElevatedButton.styleFrom(
+                    fixedSize: Size(300, 60),
+                    textStyle:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                    primary: Colors.green[700],
+                    // elevation: 15,
+                    shape: StadiumBorder()),
+              )
+            ],
+          ),
         )
       ],
     ));
